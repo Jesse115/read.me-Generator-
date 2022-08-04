7adc0d3012a0d5a5e37fc713d1fac01bee507ce6
Jesse <ftsjess@gmail.com>
2022-08-04 17:11:59 -0400

    final commit I think maybe

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
 // TODO: Include packages needed for this application
-const inquirer = require ("inquirer");
+const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
-    
-   {
-    type: 'input',
-    name: 'Title',
-    message: 'what is the title of your project'
-   },
-   {
-    type: 'input',
-    name: 'Description',
-    message: 'Description of your project'
-   },
-   {
-    type: 'input',
-    name: 'TableofContents',
-    message: 'what is in your read.me'
-   },
-   {
-    type: 'input',
-    name: 'Installation',
-    message: 'What are the steps required to install your project?'
-   },
-   {
-    type: 'input',
-    name: 'Usage',
-    message: 'Provide instructions and examples for use.'
-   },
-   {
-    type: 'input',
-    name: 'Credits',
-    message: 'List your collaborators'
-   },
-   {
-     type: 'list', 
-     message: 'Which License would you like to include?', 
-     name: 'License', 
-     choices: ["No License", "Apache 2.0 License", "Boost", "BSD 3-Clause License", "BSD 2-Clause License", "CC0", "Attribution 4.0 International", "Attribution-ShareAlike 4.0 International", "Attribution-NonCommercial 4.0 International", "Attribution-NoDerivates 4.0 International", "Attribution-NonCommmercial-ShareAlike 4.0 International", "Attribution-NonCommercial-NoDerivatives 4.0 International", "Eclipse Public License 1.0", "GNU GPL v3", "GNU GPL v2", "GNU AGPL v3", "GNU LGPL v3", "GNU FDL v1.3", "The Hippocratic License 2.1", "The Hippocratic License 3.0", "IBM Public License Version 1.0", "ISC License (ISC)", "The MIT License", "Mozilla Public License 2.0", "Attribution License (BY)", "Open Database License (ODbL)", "Public Domain Dedication and License (PDDL)", "The Perl License", "The Artistic License 2.0", "SIL Open Font License 1.1", "The Unlicense", "The Do What the Fuck You Want to Public License", "The zlib/libpng License"]
-   },
-   {
-    type: 'input',
-    name: 'Features',
-    message: 'If your project has a lot of features, list them here.'
-   },
-   {
-    type: 'input',
-    name: 'HowtoContribute',
-    message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.'
-   },
+
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'what is the title of your project'
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Description of your project'
+    },
+    {
+        type: 'input',
+        name: 'TableofContents',
+        message: 'what is in your read.me'
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide instructions and examples for use.'
+    },
+    {
+        type: 'input',
+        name: 'Credits',
+        message: 'List your collaborators'
+    },
+    {
+        type: 'list',
+        message: 'Which License would you like to include?',
+        name: 'License',
+        choices: ["No License", "Apache 2.0 License", "Boost", "BSD 3-Clause License", "BSD 2-Clause License", "CC0", "Attribution 4.0 International", "Attribution-ShareAlike 4.0 International", "Attribution-NonCommercial 4.0 International", "Attribution-NoDerivates 4.0 International", "Attribution-NonCommmercial-ShareAlike 4.0 International", "Attribution-NonCommercial-NoDerivatives 4.0 International", "Eclipse Public License 1.0", "GNU GPL v3", "GNU GPL v2", "GNU AGPL v3", "GNU LGPL v3", "GNU FDL v1.3", "The Hippocratic License 2.1", "The Hippocratic License 3.0", "IBM Public License Version 1.0", "ISC License (ISC)", "The MIT License", "Mozilla Public License 2.0", "Attribution License (BY)", "Open Database License (ODbL)", "Public Domain Dedication and License (PDDL)", "The Perl License", "The Artistic License 2.0", "SIL Open Font License 1.1", "The Unlicense", "The Do What the Fuck You Want to Public License", "The zlib/libpng License"]
+    },
+    {
+        type: 'input',
+        name: 'Features',
+        message: 'If your project has a lot of features, list them here.'
+    },
+    {
+        type: 'input',
+        name: 'HowtoContribute',
+        message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.'
+    },
+    {
+        type: 'input',
+        name: 'Questions',
+        message: 'How can someone reach you if they have questions? Please enter your information like this without parentheses (Github Username, Email Address)'
+    },
 
 
 
@@ -63,16 +68,16 @@ const questions = [
 function init() {
 
     inquirer.prompt(questions)
-    .then((choices)=>{
-        let templetereadme = generateMarkdown(choices);
-        fs.writeFile("readme.md", templetereadme, (error) => {
-         if (error) {
-            throw error
-        
-         }
-         console.log ("file sucessfully created")
-        }  )
-    })
+        .then((choices) => {
+            let templetereadme = generateMarkdown(choices);
+            fs.writeFile("readme.md", templetereadme, (error) => {
+                if (error) {
+                    throw error
+
+                }
+                console.log("file sucessfully created")
+            })
+        })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,154 +1,154 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 
 // If there is no license, return an empty string
-  function renderLicenseBadge(license) {
-    let badgeImageUrl;
-    switch (license) {
-      case "No License":
-        badgeImageUrl = ""
-        break;
-  
-      case "Apache 2.0 License":
-        badgeImageUrl = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-        break;
-  
-      case "Boost":
-        badgeImageUrl = "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-        break;
-  
-      case "BSD 3-Clause License":
-        badgeImageUrl = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-        break;
-  
-      case "BSD 2-Clause License":
-        badgeImageUrl = "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
-        break;
-  
-      case "CC0":
-        badgeImageUrl = "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
-        break;
-  
-      case "Attribution 4.0 International":
-        badgeImageUrl = "[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)";
-        break;
-  
-      case "Attribution-ShareAlike 4.0 International":
-        badgeImageUrl = "[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)";
-        break;
-  
-      case "Attribution-NonCommercial 4.0 International":
-        badgeImageUrl = "[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)";
-        break;
-  
-      case "Attribution-NoDerivates 4.0 International":
-        badgeImageUrl = "[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)";
-        break;
-  
-      case "Attribution-NonCommmercial-ShareAlike 4.0 International":
-        badgeImageUrl = "[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)";
-        break;
-  
-      case "Attribution-NonCommercial-NoDerivatives 4.0 International":
-        badgeImageUrl = "[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)";
-        break;
-  
-      case "Eclipse Public License 1.0":
-        badgeImageUrl = "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
-        break;
-  
-      case "GNU GPL v3":
-        badgeImageUrl = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-        break;
-  
-      case "GNU GPL v2":
-        badgeImageUrl = "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
-        break;
-  
-      case "GNU AGPL v3":
-        badgeImageUrl = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
-        break;
-  
-      case "GNU LGPL v3":
-        badgeImageUrl = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-        break;
-  
-      case "GNU FDL v1.3":
-        badgeImageUrl = "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";
-        break;
-  
-      case "The Hippocratic License 2.1":
-        badgeImageUrl = "[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)";
-        break;
-  
-      case "The Hippocratic License 3.0":
-        badgeImageUrl = "[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)";
-        break;
-  
-      case "IBM Public License Version 1.0":
-        badgeImageUrl = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
-        break;
-  
-      case "ISC License (ISC)":
-        badgeImageUrl = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-        break;
-  
-      case "The MIT License":
-        badgeImageUrl = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-        break;
-  
-      case "Mozilla Public License 2.0":
-        badgeImageUrl = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-        break;
-  
-      case "Attribution License (BY)":
-        badgeImageUrl = "[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)";
-        break;
-  
-      case "Open Database License (ODbL)":
-        badgeImageUrl = "[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)";
-        break;
-  
-      case "Public Domain Dedication and License (PDDL)":
-        badgeImageUrl = "[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)";
-        break;
-  
-      case "The Perl License":
-        badgeImageUrl = "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
-        break;
-  
-      case "The Artistic License 2.0":
-        badgeImageUrl = "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
-        break;
-  
-      case "SIL Open Font License 1.1":
-        badgeImageUrl = "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)";
-        break;
-  
-      case "The Unlicense":
-        badgeImageUrl = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
-        break;
-  
-      case "The Do What the Fuck You Want to Public License":
-        badgeImageUrl = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
-        break;
-  
-      case "The zlib/libpng License":
-        badgeImageUrl = "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)";
-        break;
-  
-      default:
-        return "";
-    }
-    return badgeImageUrl;
+function renderLicenseBadge(license) {
+  let badgeImageUrl;
+  switch (license) {
+    case "No License":
+      badgeImageUrl = ""
+      break;
+
+    case "Apache 2.0 License":
+      badgeImageUrl = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+
+    case "Boost":
+      badgeImageUrl = "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+      break;
+
+    case "BSD 3-Clause License":
+      badgeImageUrl = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+
+    case "BSD 2-Clause License":
+      badgeImageUrl = "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+      break;
+
+    case "CC0":
+      badgeImageUrl = "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
+      break;
+
+    case "Attribution 4.0 International":
+      badgeImageUrl = "[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)";
+      break;
+
+    case "Attribution-ShareAlike 4.0 International":
+      badgeImageUrl = "[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)";
+      break;
+
+    case "Attribution-NonCommercial 4.0 International":
+      badgeImageUrl = "[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)";
+      break;
+
+    case "Attribution-NoDerivates 4.0 International":
+      badgeImageUrl = "[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)";
+      break;
+
+    case "Attribution-NonCommmercial-ShareAlike 4.0 International":
+      badgeImageUrl = "[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)";
+      break;
+
+    case "Attribution-NonCommercial-NoDerivatives 4.0 International":
+      badgeImageUrl = "[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)";
+      break;
+
+    case "Eclipse Public License 1.0":
+      badgeImageUrl = "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+      break;
+
+    case "GNU GPL v3":
+      badgeImageUrl = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+
+    case "GNU GPL v2":
+      badgeImageUrl = "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+      break;
+
+    case "GNU AGPL v3":
+      badgeImageUrl = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+      break;
+
+    case "GNU LGPL v3":
+      badgeImageUrl = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+      break;
+
+    case "GNU FDL v1.3":
+      badgeImageUrl = "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";
+      break;
+
+    case "The Hippocratic License 2.1":
+      badgeImageUrl = "[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)";
+      break;
+
+    case "The Hippocratic License 3.0":
+      badgeImageUrl = "[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)";
+      break;
+
+    case "IBM Public License Version 1.0":
+      badgeImageUrl = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+      break;
+
+    case "ISC License (ISC)":
+      badgeImageUrl = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+      break;
+
+    case "The MIT License":
+      badgeImageUrl = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+
+    case "Mozilla Public License 2.0":
+      badgeImageUrl = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+
+    case "Attribution License (BY)":
+      badgeImageUrl = "[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)";
+      break;
+
+    case "Open Database License (ODbL)":
+      badgeImageUrl = "[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)";
+      break;
+
+    case "Public Domain Dedication and License (PDDL)":
+      badgeImageUrl = "[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)";
+      break;
+
+    case "The Perl License":
+      badgeImageUrl = "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+      break;
+
+    case "The Artistic License 2.0":
+      badgeImageUrl = "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+      break;
+
+    case "SIL Open Font License 1.1":
+      badgeImageUrl = "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)";
+      break;
+
+    case "The Unlicense":
+      badgeImageUrl = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+      break;
+
+    case "The Do What the Fuck You Want to Public License":
+      badgeImageUrl = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+      break;
+
+    case "The zlib/libpng License":
+      badgeImageUrl = "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)";
+      break;
+
+    default:
+      return "";
   }
+  return badgeImageUrl;
+}
 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license){
+function renderLicenseLink(license) {
   let licenseLink;
   switch (license) {
-    
+
     case "No License":
       badgeImageUrl = ""
       break;
@@ -290,7 +290,7 @@ function renderLicenseLink(license){
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license==="No License"){
+  if (license === "No License") {
     return "";
   }
   return `## License\n
@@ -299,6 +299,7 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let Contact = data.Questions.split(",");
   return `# ${data.Title}
   ${renderLicenseBadge(data.License)}
   ## Description 
@@ -325,7 +326,12 @@ function generateMarkdown(data) {
   ## How to Contribute
   ${data.HowtoContribute}
 
-`; 
+  ## Questions 
+  Github: [@${Contact[0]}](https://www.github.com/${Contact[0]})\n
+  Email: ${Contact[1]}\n
+
+
+`;
 }
 
 module.exports = generateMarkdown;
